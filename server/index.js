53e95f7646e7445289ce41bd79b1f45ff3b1425b
Peter Lamberg <pgithub@pelam.fi>
2020-06-07 18:42:20 +0300

    Basic server api

--- server/index.js
+++ server/index.js
@@ -19,45 +19,60 @@ pool.on('error', (err, client) => {
     console.error('Unexpected error on idle client', err)
 })
 
-async function login(req, res) {
+async function getScriptlet(req, res) {
     const { workshopId, userName } = req.params
+    console.log("getScriptlet request", workshopId, userName)
+    const client = await pool.connect()
+    try {
 
-    const { rows } = await client.query(
-        `select scriptlet from code_canvas_entry where id = 
-        (select max (id) from code_canvas_entry where workshop_id = $1, nickname = $2)`, [workshopId, userId])
+        const { rows } = await client.query(
+            `select scriptlet from code_canvas_entry where id = 
+            (select max (id) from code_canvas_entry where workshop_id = $1 and nickname = $2);`, [workshopId, userName])
 
-    if (rows.length == 0) {
-        const { result } = await client.query(`insert into code_canvas_entry (workshop_id, nickname, scriptlet) 
-        values ($1, $2, 'x = t'`, [workshopId, userName]);
+        if (rows.length == 0) {
+            const scriptlet = 'x = t'    
+            const { result } = await client.query(`insert into code_canvas_entry (workshop_id, nickname, scriptlet) 
+            values ($1, $2, $3)`, [workshopId, userName, scriptlet]);
+            res.send({scriptlet})
+        } else {
+            res.send(rows[0])
+        }
+    } catch (error) {
+        console.log("login error", workshopId, userName, error)    
+        res.status(500).send("login error")
     }
-        
-    res.send(rows)
+    finally {
+        client.release()
+    }
+}
+
+async function saveScriptlet(req, res) {
+    const { workshopId, userName } = req.params
+    const scriptlet = req.body.toString('utf8')
+    console.log("saveScriptlet request", workshopId, userName, scriptlet.split(/[^- \t;+.,\(\)a-zA-Z=0-9]/).join("#").substring(0, 32))
+    const client = await pool.connect()
+  
+    try {
+        await client.query(`insert into code_canvas_entry (workshop_id, nickname, scriptlet) 
+        values ($1, $2, $3)`, [workshopId, userName, scriptlet]);
+        res.send({result: 'ok'})
+
+    } catch (error) {
+        console.log("save error", workshopId, userName, error)    
+        res.status(500).send("save error")
+    }
+    finally {
+        client.release()
+    }
 }
 
 express()
+  .use(express.raw({type: "*/*"}))
+  //.use(express.json()) // for parsing application/json
   .use(express.static(path.join(__dirname, 'public')))
   .get('/', (req, res) => res.render('public/index'))
-  .get(/\/worshop\/[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}/, (req, res) => res.render('public/index'))
-  .get(/\/worshop\/[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}\/:username/, (req, res) => res.render('public/index'))
-  .get('/api/0/login', login)
+  .get(/\/workshop\/[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}/, (req, res) => res.render('public/index'))
+  .get(/\/workshop\/[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}\/:userName/, (req, res) => res.render('public/index'))
+  .get('/api/0/:workshopId([0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12})/:userName/', getScriptlet)
+  .post('/api/0/:workshopId([0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12})/:userName/', saveScriptlet)
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
-
-async function test() {
-  const client = await pool.connect()
-  try {
-    const res = await client.query('SELECT table_schema,table_name FROM information_schema.tables;')
-    console.log(JSON.stringify(res.rows, null, 0));
-  } finally {
-    client.release()
-  }
-}
-
-   
-/*
-const workshopId = '8a7f0b49-f389-44c4-ab12-0010d7a94b6b';    
-const { result } = await client.query(`insert into code_canvas_entry (workshop_id, nickname, scriptlet) 
-    values ($1, $2, 'x = t'`, [workshopId, userName]);
-*/
-
-test()
-.catch(console.error)
