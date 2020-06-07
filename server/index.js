ee600e74cf2582a28918c24cdce954d85eec9c2a
Peter Lamberg <pgithub@pelam.fi>
2020-06-07 09:54:08 +0300

    experimental pg support in server

--- server/index.js
+++ server/index.js
@@ -6,3 +6,24 @@ express()
   .use(express.static(path.join(__dirname, 'public')))
   .get('/', (req, res) => res.render('public/index'))
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+
+
+const { Client } = require('pg');
+
+const client = new Client({
+    connectionString: process.env.DATABASE_URL,
+    ssl: {
+	rejectUnauthorized: false
+    }
+});
+
+client.connect();
+
+client.query('SELECT table_schema,table_name FROM information_schema.tables;', (err, res) => {
+    if (err) throw err;
+    for (let row of res.rows) {
+	console.log(JSON.stringify(row));
+    }
+    client.end();
+});
+
